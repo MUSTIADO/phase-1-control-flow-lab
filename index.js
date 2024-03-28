60d519a6e6464d9ad9b5d25a9183088aa9a7f829
Mustafa Adan Maalim <162093953+MUSTIADO@users.noreply.github.com>
2024-03-28 12:37:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function scuberGreetingForFeet(num) {
-  if (num <= 400) {
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
     return "This one is on me!";
-  } else if (num <= 2000) {
+  } else if (feet <= 2000) {
     return "That will be twenty bucks.";
-  } else if (num <= 2500) {
+  } else if (feet <= 2500) {
     return "I will gladly take your thirty bucks.";
   } else {
     return "No can do.";
